0ebba2f553d5e6e19aac9e48aacbfd961ad057ed
Stav Laufer <stav.lau@gmail.com>
2014-11-03 21:51:57 +0200

    Exe 02 - added a calculation

--- src/com/mta/javacourse/Java_MISServlet.java
+++ src/com/mta/javacourse/Java_MISServlet.java
@@ -5,8 +5,14 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Java_MISServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		String name = "Stav";
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, " + name);
+		resp.setContentType("text/html");
+		
+		int num1 = 4, num2 = 3, num3 = 7;
+		
+		int result = (num1 + num2) * num3;
+		
+		String resultStr = new String("<h1>Result of " + num1 + "+" + num2 + "*" + num3 + "=" + result + "</h1>");
+		
+		resp.getWriter().println(resultStr);
 	}
 }
