a95c800a935846b6fd2d7c6cc7bc91f4c5a63eec
Stav Laufer <stav.lau@gmail.com>
2014-11-06 10:41:29 +0200

    Exe 2 - fixed brackets in the calculation

--- src/com/mta/javacourse/Java_MISServlet.java
+++ src/com/mta/javacourse/Java_MISServlet.java
@@ -11,7 +11,7 @@ public class Java_MISServlet extends HttpServlet {
 		
 		int result = (num1 + num2) * num3;
 		
-		String resultStr = new String("<h1>Result of " + num1 + "+" + num2 + "*" + num3 + "=" + result + "</h1>");
+		String resultStr = new String("<h1>Result of (" +num1 + "+" + num2 + ")*" + num3 + "=" + result + "</h1>");
 		
 		resp.getWriter().println(resultStr);
 	}
